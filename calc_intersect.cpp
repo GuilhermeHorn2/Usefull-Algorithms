bae68cda3c7932fc29d5f2af9cec776614ac37ec
Guilherme Azevedo Horn <azevedo.horn@hotmail.com>
2023-08-15 13:32:42 -0300

    Update calc_intersect.cpp

--- calc_intersect.cpp
+++ calc_intersect.cpp
@@ -3,10 +3,13 @@
 #include <vector>
 #include <set>
 #include <unordered_set>
+#include <algorithm>
 
 int calc_intersect_1(std::vector<int> &a, std::vector<int> &b);
 
 int calc_intersect_2(std::vector<int> &a, std::vector<int> &b);
+
+int calc_intersect_3(std::vector<int> &a, std::vector<int> &b);
 
 int main()
 {
@@ -16,7 +19,9 @@ int main()
 
  std::cout << calc_intersect_1(a, b) << '\n';
 
- std::cout << calc_intersect_2(a, b);
+ std::cout << calc_intersect_2(a, b) << '\n';
+
+ std::cout << calc_intersect_3(a, b) << '\n';
 
  return 0;
 }
@@ -52,5 +57,29 @@ int calc_intersect_2(std::vector<int> &a, std::vector<int> &b)
   count += set_b.count(x);
  }
 
+ return count;
+}
+
+int calc_intersect_3(std::vector<int> &a, std::vector<int> &b)
+{
+ // assuming that both lists have unique elements
+ //  O(nlogn),but for some reason this is actually faster than the algorithm 2
+
+ for (int i = 0; i < b.size(); i++)
+ {
+  a.push_back(b[i]);
+ }
+
+ std::sort(a.begin(), a.end());
+ // std::sort(b.begin(), b.end());
+
+ int count = 0;
+ for (int i = 1; i < a.size(); i++)
+ {
+  if (a[i] == a[i - 1])
+  {
+   count++;
+  }
+ }
  return count;
 }
